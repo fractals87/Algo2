379a9c2645f8a0dcd4b49caddca2ac5e830bcb55
3PICTSOFTWAREDev <sviluppo@3p-ictsoftware.it>
2021-10-08 23:57:07 +0200

    backup

--- src/upo/graph20018562/AdjListUndir.java
+++ src/upo/graph20018562/AdjListUndir.java
@@ -1,138 +1,367 @@
 package upo.graph20018562;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
 
 import upo.graph.base.*;
+import upo.graph.base.VisitForest.Color;
+import upo.graph.base.VisitForest.VisitType;
 
 /**
  * Implementazione mediante <strong>liste di adiacenza</strong> di un grafo <strong>non orientato non pesato</strong>.
  * 
- * @author Nome Cognome Matricola
+ * @author Paolo Franzini 20018562
  *
  */
 public class AdjListUndir implements Graph{
+	
+	private List<LinkedList<Integer>> vertexList;	
+	public boolean log; //Used only for view debug
+	
+	public AdjListUndir() {		
+		vertexList=new LinkedList<LinkedList<Integer>>();	
+		log = false;
+	}
 
 	@Override
 	public int addVertex() {
-		// TODO Auto-generated method stub
-		return 0;
+		LinkedList<Integer> adjacentVertex=new LinkedList<Integer>(); 
+		vertexList.add(this.size(),adjacentVertex);
+		return this.size();
 	}
 
 	@Override
 	public boolean containsVertex(int index) {
-		// TODO Auto-generated method stub
-		return false;
+		if(this.size()>index) return true;
+		else return false;
 	}
 
 	@Override
 	public void removeVertex(int index) throws NoSuchElementException {
-		// TODO Auto-generated method stub
+		if(containsVertex(index)==false) throw new NoSuchElementException();
+		vertexList.remove(index);
 		
+		for(int i=0; i<this.size(); i++) {
+			for(int y=0; y<vertexList.get(i).size(); y++) {
+				if(vertexList.get(i).get(y)==index) { 
+					vertexList.get(i).remove(y);
+				    y--; 	
+				}
+				else if(vertexList.get(i).get(y)>index)
+					vertexList.get(i).set(y, (vertexList.get(i).get(y))-1); 
+			}
+		}
 	}
 
 	@Override
 	public void addEdge(int sourceVertexIndex, int targetVertexIndex) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+		 if(containsVertex(sourceVertexIndex)==false || containsVertex(targetVertexIndex)==false) throw new IllegalArgumentException();
+		 
+		 vertexList.get(sourceVertexIndex).add(targetVertexIndex);
+		 vertexList.get(targetVertexIndex).add(sourceVertexIndex);
 		
 	}
 
 	@Override
 	public boolean containsEdge(int sourceVertexIndex, int targetVertexIndex) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return false;
+		 if(containsVertex(sourceVertexIndex)==false || containsVertex(targetVertexIndex)==false) throw new IllegalArgumentException();
+		 
+		 if(vertexList.get(sourceVertexIndex).contains(targetVertexIndex) && vertexList.get(targetVertexIndex).contains(sourceVertexIndex)) return true;
+		 else return false;
 	}
 
 	@Override
-	public void removeEdge(int sourceVertexIndex, int targetVertexIndex)
-			throws IllegalArgumentException, NoSuchElementException {
-		// TODO Auto-generated method stub
+	public void removeEdge(int sourceVertexIndex, int targetVertexIndex) {
+		if(containsVertex(sourceVertexIndex)==false || containsVertex(targetVertexIndex)==false) 
+			return;
+	 
+		if(vertexList.get(sourceVertexIndex).contains(targetVertexIndex)==false)
+			return;
 		
+		vertexList.get(sourceVertexIndex).remove(vertexList.get(sourceVertexIndex).indexOf(targetVertexIndex));
+		vertexList.get(targetVertexIndex).remove(vertexList.get(targetVertexIndex).indexOf(sourceVertexIndex));		
 	}
 
 	@Override
 	public Set<Integer> getAdjacent(int vertexIndex) throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if(containsVertex(vertexIndex)==false) throw new NoSuchElementException();
+		
+		Set<Integer> adjacent=new HashSet<Integer>();
+		
+		for(Integer ver : vertexList.get(vertexIndex))
+			adjacent.add(ver);
+		
+		return adjacent;
 	}
 
 	@Override
 	public boolean isAdjacent(int targetVertexIndex, int sourceVertexIndex) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return false;
+		if(containsVertex(sourceVertexIndex)==false || containsVertex(targetVertexIndex)==false) 
+			 throw new IllegalArgumentException();
+		
+		return vertexList.get(sourceVertexIndex).contains(targetVertexIndex);
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return vertexList.size();
 	}
 
 	@Override
 	public boolean isDirected() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isCyclic() {
-		// TODO Auto-generated method stub
+		VisitForest forest=new VisitForest(this, VisitType.DFS_TOT);
+		for(int i=0; i<this.size(); i++) {
+			if(forest.getColor(i)==Color.WHITE && VisitaRicCiclo(forest, i))
+				return true;
+		}
+		return false;
+	}
+	
+	private boolean VisitaRicCiclo(VisitForest forest, int u){
+		forest.setColor(u, Color.GRAY);
+		for(Integer v : this.getAdjacent(u)) {
+			if(forest.getColor(v)==Color.WHITE) {
+				forest.setParent(v, u);
+				if(VisitaRicCiclo(forest, v)) return true;
+			}
+			else if(v!=forest.getPartent(u)) return true;
+		}
+		forest.setColor(u, Color.BLACK);
 		return false;
 	}
 
 	@Override
 	public boolean isDAG() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public VisitForest getBFSTree(int startingVertex) throws UnsupportedOperationException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.containsVertex(startingVertex)==false) throw new IllegalArgumentException();
+		
+		VisitForest forest=new VisitForest(this, VisitType.BFS);
+	
+		LinkedList<Integer> frangia=new LinkedList<Integer>();
+		forest.setColor(startingVertex, Color.GRAY);
+		frangia.add(startingVertex);
+		Integer u;
+		while(!frangia.isEmpty()) {
+			u=frangia.get(0); 	
+			if(log)
+				System.out.print(u + "-");
+			for(Integer v : vertexList.get(u)) {
+				if(forest.getColor(v)==Color.WHITE) {
+					forest.setColor(v, Color.GRAY);  	
+					forest.setParent(v, u);
+				    frangia.add(v);
+				}
+			}
+			forest.setColor(u, Color.BLACK);
+			frangia.remove(0);
+		}
+		if(log)
+			System.out.print("\n");			
+		return forest;
 	}
 
 	@Override
 	public VisitForest getDFSTree(int startingVertex) throws UnsupportedOperationException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		if(this.containsVertex(startingVertex)==false) throw new IllegalArgumentException();
+		
+		LinkedList<Integer> frangia=new LinkedList<Integer>();
+		Integer[] arrTop=new Integer[this.size()];
+				
+		for(int i=0; i<this.size(); i++) 
+			arrTop[i]=0;
+		
+		VisitForest forest=new VisitForest(this, VisitType.DFS);
+		
+		forest.setColor(startingVertex, Color.GRAY); 
+		if(log)
+			System.out.print(startingVertex+"-");
+		frangia.add(startingVertex); 
+		Integer v;
+		while(!frangia.isEmpty()) {
+			while(arrTop[frangia.get(0)]!=null) {
+				v=vertexList.get(frangia.get(0)).get(arrTop[frangia.get(0)]);
+				arrTop[frangia.get(0)]++;
+				if(arrTop[frangia.get(0)]==vertexList.get(frangia.get(0)).size())
+					arrTop[frangia.get(0)]=null;
+				if(forest.getColor(v)==Color.WHITE) {
+					if(log)
+						System.out.print(v+"-");
+					forest.setColor(v, Color.GRAY);
+					forest.setParent(v, frangia.get(0));
+					frangia.add(0, v);
+				}
+			}	
+			forest.setColor(frangia.get(0), Color.BLACK);
+			frangia.remove(0);
+		}
+		if(log)
+			System.out.print("\n");		
+		return forest;
 	}
 
 	@Override
-	public VisitForest getDFSTOTForest(int startingVertex)
-			throws UnsupportedOperationException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+	public VisitForest getDFSTOTForest(int startingVertex) throws UnsupportedOperationException, IllegalArgumentException {
+		VisitForest forest=new VisitForest(this, VisitType.DFS_TOT);
+		for(int i=0; i<this.size(); i++) {
+			if(forest.getColor(i)==Color.WHITE) {
+				if(log)
+					System.out.print("Visito: " + i + "\n");
+					VisitDFS(forest, i);
+			}else {
+				if(log)
+					System.out.print("Salto: " + i + "\n");
+			}
+				
+		}
+		return forest;
+	}
+	
+	public void VisitDFS(VisitForest forest,int startingVertex) {
+		LinkedList<Integer> frangia=new LinkedList<Integer>();
+		Integer[] arrTop=new Integer[this.size()];
+				
+		for(int i=0; i<this.size(); i++) 
+			arrTop[i]=0;
+		
+		forest.setColor(startingVertex, Color.GRAY); 
+		if(log)
+			System.out.print(startingVertex+"-");
+		frangia.add(startingVertex); 
+		Integer v;
+		while(!frangia.isEmpty()) {
+			while(arrTop[frangia.get(0)]!=null) {
+				v=vertexList.get(frangia.get(0)).get(arrTop[frangia.get(0)]);
+				arrTop[frangia.get(0)]++;
+				if(arrTop[frangia.get(0)]==vertexList.get(frangia.get(0)).size())
+					arrTop[frangia.get(0)]=null;
+				if(forest.getColor(v)==Color.WHITE) {
+					if(log)
+						System.out.print(v+"-");
+					forest.setColor(v, Color.GRAY);
+					forest.setParent(v, frangia.get(0));
+					frangia.add(0, v);
+				}
+			}	
+			forest.setColor(frangia.get(0), Color.BLACK);
+			frangia.remove(0);
+		}
+		if(log)
+			System.out.print("\n");		
 	}
 
 	@Override
-	public VisitForest getDFSTOTForest(int[] vertexOrdering)
-			throws UnsupportedOperationException, IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+	public VisitForest getDFSTOTForest(int[] vertexOrdering) throws UnsupportedOperationException, IllegalArgumentException {
+		
+		VisitForest forest=new VisitForest(this, VisitType.DFS_TOT);
+		//Visit before order required
+		for(int i=0; i<vertexOrdering.length; i++) {
+			if(forest.getColor(vertexOrdering[i])==Color.WHITE) {
+				if(log)
+					System.out.print("Visito in ordine: " + vertexOrdering[i] + "\n");
+				VisitDFS(forest, vertexOrdering[i]);
+			}else {
+				if(log)
+					System.out.print("Salto: " + vertexOrdering[i] + "\n");
+			}
+				
+		}
+		
+		//After only white vertex not connected and not started from before visit
+		for(int i=0; i<this.size(); i++) {
+			if(forest.getColor(i)==Color.WHITE) {
+				if(log)
+					System.out.print("Visito: " + i + "\n");
+				VisitDFS(forest, i);
+			}else {
+				if(log)
+					System.out.print("Salto: " + i + "\n");
+			}				
+		}
+		return forest;
+
 	}
 
 	@Override
 	public int[] topologicalSort() throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Set<Set<Integer>> stronglyConnectedComponents() throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Set<Set<Integer>> connectedComponents() throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+		VisitForest forest=new VisitForest(this, VisitType.DFS_TOT);
+		Set<Set<Integer>> connectedComponents = new HashSet<>();
+		for(int i=0; i<this.size(); i++) {
+			if(forest.getColor(i)==Color.WHITE) 
+				connectedComponents.add(VisitConnectedComponents(forest, i));
+		}
+		return connectedComponents;
 	}
 	
+	private Set<Integer> VisitConnectedComponents(VisitForest forest, int startingVertex) {
+		Set<Integer> connectedComponent = new HashSet<>();
+		LinkedList<Integer> frangia=new LinkedList<Integer>();
+		Integer[] arrTop=new Integer[this.size()];
+				
+		for(int i=0; i<this.size(); i++) 
+			arrTop[i]=0;
+		
+		forest.setColor(startingVertex, Color.GRAY); 
+		frangia.add(startingVertex); 
+		connectedComponent.add(startingVertex);
+		Integer v;
+		while(!frangia.isEmpty()) {
+			while(arrTop[frangia.get(0)]!=null) {
+				v=vertexList.get(frangia.get(0)).get(arrTop[frangia.get(0)]);
+				arrTop[frangia.get(0)]++;
+				if(arrTop[frangia.get(0)]==vertexList.get(frangia.get(0)).size())
+					arrTop[frangia.get(0)]=null;
+				if(forest.getColor(v)==Color.WHITE) {
+					forest.setColor(v, Color.GRAY);
+					forest.setParent(v, frangia.get(0));
+					connectedComponent.add(v);
+					frangia.add(0, v);
+				}
+			}	
+			forest.setColor(frangia.get(0), Color.BLACK);
+			frangia.remove(0);
+		}
+		return connectedComponent;
+	}
+
 	public boolean equals(Object o) {
+		AdjListUndir g = (AdjListUndir)o;
+		if(g.size() != this.size())
+			return false;
+		for(int i=0; i<this.size(); i++) {
+			for(int y=0; y<vertexList.get(i).size(); y++) {
+				if(g.containsEdge(i, vertexList.get(i).get(y))==false)
+					return false;
+			}
+		}
 		return true;
 	}
 	
+	public void viewGraph() {
+		for(int i=0; i<vertexList.size(); i++) {
+			System.out.println("Vertex: "+ (i) +", adjacentVertex : "+ vertexList.get(i));
+		}
+	}
 
 }
--- src/upo/graph20018562/testCase.java
+++ src/upo/graph20018562/testCase.java
@@ -2,17 +2,284 @@ package upo.graph20018562;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
 
-import upo.graph20018562.*;
+import org.junit.Test;
 
 public class testCase {
 
 	@Test
-	public void test() {
+	public void testAddVertex() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		assertEquals(1,adjListUndir.addVertex());
+		assertEquals(2,adjListUndir.addVertex());
+		
+	}
+	
+	@Test
+	public void testContainsVertex() {
 		AdjListUndir adjListUndir = new AdjListUndir();
 		adjListUndir.addVertex();
-		assertFalse(false);
+		adjListUndir.addVertex();
+		assertEquals(true,adjListUndir.containsVertex(0));
+		assertEquals(true,adjListUndir.containsVertex(1));
+		assertEquals(false,adjListUndir.containsVertex(2));
 	}
+	
+	@Test
+	public void testAddEdge() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		
+		assertEquals(true,adjListUndir.containsEdge(0, 1));
+		assertEquals(true,adjListUndir.containsEdge(1, 2));
+		assertEquals(false,adjListUndir.containsEdge(0, 2));		
+		
+		//System.out.print("DIM:" + adjListUndir.size() + "\n");
+		//adjListUndir.viewGraph();
+	}
+	
+	@Test
+	public void testRemoveEdge() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(0, 2);
+		//adjListUndir.viewGraph();
+		adjListUndir.removeEdge(0, 1);
+		
+		assertEquals(false,adjListUndir.containsEdge(0, 1));
+		assertEquals(true,adjListUndir.containsEdge(1, 2));
+		assertEquals(true,adjListUndir.containsEdge(0, 2));		
+		
+		//System.out.print("DIM:" + adjListUndir.size() + "\n");
+		//adjListUndir.viewGraph();
+	}
+	
+	@Test
+	public void testRemoveVertex() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(0, 2);
+		//adjListUndir.viewGraph();
+		adjListUndir.removeVertex(1);
 
+		assertEquals(true,adjListUndir.containsVertex(0));
+		assertEquals(true,adjListUndir.containsVertex(1));
+		assertEquals(false,adjListUndir.containsVertex(2));
+		assertEquals(true,adjListUndir.containsEdge(0, 1));
+
+		try {
+			adjListUndir.containsEdge(0, 2);
+			fail();
+		}catch(IllegalArgumentException e) {
+			assertEquals(true,true);
+		}
+
+		//System.out.print("\n");
+		//adjListUndir.viewGraph();
+	}
+	
+	@Test
+	public void testGetAdjacent() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(0, 2);
+		
+		Set<Integer> hsTest=new HashSet<Integer>();
+		hsTest.add(1);
+		assertFalse(hsTest.equals(adjListUndir.getAdjacent(0)));
+		hsTest.add(2);
+		assertEquals(adjListUndir.getAdjacent(0),hsTest );
+	}
+	
+	@Test
+	public void testIsAdjacent() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		
+		assertEquals(true, adjListUndir.isAdjacent(0, 1));
+		assertEquals(false, adjListUndir.isAdjacent(0, 2));		
+	}
+	
+	@Test
+	public void testIsCyclic() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(0, 2);
+		
+		assertEquals(true, adjListUndir.isCyclic());
+		
+		adjListUndir.removeEdge(1, 2);
+		//adjListUndir.viewGraph();
+		assertEquals(false, adjListUndir.isCyclic());		
+	}
+	
+	@Test
+	public void testGetBFSTree() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(2, 3);
+		adjListUndir.addEdge(3, 4);
+		adjListUndir.addEdge(4, 5);
+		adjListUndir.addEdge(5, 6);
+		
+		adjListUndir.addEdge(0, 7);
+		adjListUndir.addEdge(1, 6);
+		adjListUndir.addEdge(7, 2);
+		adjListUndir.addEdge(7, 4);
+		//adjListUndir.viewGraph();
+		//djListUndir.log = true;
+		adjListUndir.getBFSTree(0);
+		//System.out.print("\nvisita attesa\n");
+		//System.out.print("0-1-7-2-6-4-3-5-\n");		
+	}
+	
+	@Test
+	public void testGetDSFTree() {
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(2, 3);
+		adjListUndir.addEdge(3, 4);
+		adjListUndir.addEdge(4, 5);
+		adjListUndir.addEdge(5, 6);
+		
+		adjListUndir.addEdge(0, 7);
+		adjListUndir.addEdge(1, 6);
+		adjListUndir.addEdge(7, 2);
+		adjListUndir.addEdge(7, 4);
+		//adjListUndir.viewGraph();
+		//adjListUndir.log = true;
+		adjListUndir.getDFSTree(0);
+		//System.out.print("\nvisita attesa\n");
+		//System.out.print("0-1-2-3-4-5-6-7-\n");		
+	}
+	
+	@Test
+	public void testGetDFSTOTForest() {
+		
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(3, 4);
+		adjListUndir.addEdge(5, 6);
+		
+		int[] myOrder = {2, 3};
+		//adjListUndir.viewGraph();
+		//adjListUndir.log = true;
+		adjListUndir.getDFSTOTForest(myOrder);
+		/*
+		System.out.print("\nvisita attesa\n");
+		System.out.print("Visito 2-1-0-\n");	
+		System.out.print("Visito 3-4-\n");	
+		System.out.print("Salto 0-1-2-3-4-\n");	
+		System.out.print("Visito 5-6\n");
+		System.out.print("Salto 6\n");			
+		*/
+	}
+	
+	@Test
+	public void testConnectedComponents() {
+		
+		AdjListUndir adjListUndir = new AdjListUndir();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addVertex();	
+		adjListUndir.addEdge(0, 1);
+		adjListUndir.addEdge(1, 2);
+		adjListUndir.addEdge(3, 4);
+		adjListUndir.addEdge(5, 6);
+		
+		//adjListUndir.viewGraph();
+		//adjListUndir.log = true;
+		Set<Set<Integer>> connectedComponents = adjListUndir.connectedComponents();
+		
+		for(Set<Integer> component : connectedComponents){
+			for(Integer index : component) {
+				//System.out.print(index + "-");		
+			}
+			//System.out.print("\n");		
+		}
+		/*
+		System.out.print("Atteso\n");
+		System.out.print("0-1-2\n");
+		System.out.print("3-4-\n");	
+		System.out.print("5-6-\n");	*/
+	}
+	
+	@Test
+	public void equals() {
+		
+		AdjListUndir adjListUndir1 = new AdjListUndir();
+		adjListUndir1.addVertex();
+		adjListUndir1.addVertex();
+		adjListUndir1.addVertex();
+		adjListUndir1.addEdge(0, 1);
+		adjListUndir1.addEdge(1, 2);
+		adjListUndir1.addEdge(2, 0);		
+		AdjListUndir adjListUndir2 = new AdjListUndir();
+		adjListUndir2.addVertex();
+		adjListUndir2.addVertex();
+		adjListUndir2.addVertex();
+		adjListUndir2.addEdge(2, 0);
+		adjListUndir2.addEdge(1, 2);		
+		adjListUndir2.addEdge(0, 1);
+		
+		assertEquals(true,adjListUndir1.equals(adjListUndir2) );
+	}
 }
